61ffe58f46c6682058ba0606f54d7485d0fe4d7c
ADI NARAYANA REDDY <68352319+Adi19471@users.noreply.github.com>
2026-02-20 18:04:13 +0530

    Add command to create superuser automatically
    s

--- slots/management/commands/createsu.py
+++ slots/management/commands/createsu.py
@@ -1,1 +1,17 @@
 
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = "Create superuser automatically"
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
+            self.stdout.write("Superuser created")
+        else:
+            self.stdout.write("Superuser already exists")
